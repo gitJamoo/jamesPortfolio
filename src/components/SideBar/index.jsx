69addbfbfeb7f0cbf8ae6a11445560df4425745c
James <33129549+gitJamoo@users.noreply.github.com>
2022-03-02 15:01:28 -0800

    customized channel bar

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -6,11 +6,11 @@ const SideBar = () => {
     <div className="fixed top-0 left-0 h-screen w-16 flex flex-col
                   bg-white dark:bg-gray-900 shadow-lg">
                     
-        <SideBarIcon icon={<FaFire size="28" />} />
+        <SideBarIcon icon={<FaFire size="28" />}text={"fiyaaaaaaaaaaaaa"} />
         <Divider />
-        <SideBarIcon icon={<BsPlus size="32" />} />
-        <SideBarIcon icon={<BsFillLightningFill size="20" />} />
-        <SideBarIcon icon={<FaPoo size="20" />} />
+        <SideBarIcon icon={<BsPlus size="32" />}text={"plus"} />
+        <SideBarIcon icon={<BsFillLightningFill size="20" />}text={"its lit"} />
+        <SideBarIcon icon={<FaPoo size="20" />}text={"poop.exe"} />
         <Divider />
         <SideBarIcon icon={<BsGearFill size="22" />} text={"settings"} />
     </div>
