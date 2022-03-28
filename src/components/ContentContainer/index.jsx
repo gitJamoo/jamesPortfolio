37d6280c2a96020ca3355a415e09f43d37cc217c
James <33129549+gitJamoo@users.noreply.github.com>
2022-03-28 14:43:09 -0700

    sonic high school

--- src/components/ContentContainer/index.jsx
+++ src/components/ContentContainer/index.jsx
@@ -4,7 +4,8 @@ import { BsPlusCircleFill } from 'react-icons/bs';
 
 const ContentContainer = () => {
   return (
-    <div className='content-container'>
+   <a href="https://www.fanfiction.net/s/8085414/1/SONIC-HIGH-SCHOOL">
+   <div className='content-container'>
       <TopNavigation />
       <div className='content-list'>
         <Custom
@@ -63,6 +64,7 @@ const ContentContainer = () => {
       </div>
       <BottomBar />
     </div>
+    </a>
   );
 };
 
